5c81ff0ea1605a200c7d5d001a620c8e900f61cd
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2019-12-03 15:22:38 -0700

    Add data file
    Co-authored-by: Vanessa Randall <vanessalrandall@gmail.com>

---
+++ data/data.js
@@ -0,0 +1,40 @@
+const data = {
+  projects: [
+    {
+      project_name: 'Portfolio Website'
+    },
+    {
+      project_name: 'Nature'
+    }
+  ],
+  palettes: [
+    {
+      palette_name: 'Option 1',
+      color_1: '#192435',
+      color_2: '#678589',
+      color_3: '#77ACA2',
+      color_4: '#EDF3F3',
+      color_5: '#C59563',
+      project_name: 'Portfolio Website'
+    },
+    {
+      palette_name: 'Option 2',
+      color_1: '#D8E2DC',
+      color_2: '#FFE5D9',
+      color_3: 'D1A6AE',
+      color_4: '#9D8189',
+      color_5: '#432F32',
+      project_name: 'Portfolio Website'
+    },
+    {
+      palette_name: 'Ocean',
+      color_1: '#020216',
+      color_2: '#00042B',
+      color_3: '#001E64',
+      color_4: '#006CAD',
+      color_5: '#00C0FA',
+      project_name: 'Nature'
+    },
+
+  ]
+}
