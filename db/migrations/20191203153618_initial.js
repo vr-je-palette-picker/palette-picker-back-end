2229f6204e6717097c673a6a823e31394740ad22
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2019-12-03 16:07:42 -0700

    Add latest migration
    Co-authored-by: Vanessa Randall <vanessalrandall@gmail.com>

--- db/migrations/20191203153618_initial.js
+++ db/migrations/20191203153618_initial.js
@@ -1,8 +1,33 @@
+exports.up = knex => {
+  return Promise.all([
+    knex.schema.createTable('projects', table => {
+      table.increments('id').primary();
+      table.string('project_name');
+      
+      table.timestamps(true, true);
+    }),
 
-exports.up = function(knex) {
-  
+    knex.schema.createTable('palettes', table => {
+      table.increments('id').primary();
+      table.string('palette_name');
+      table.string('color_1');
+      table.string('color_2');
+      table.string('color_3');
+      table.string('color_4');
+      table.string('color_5');
+      table.integer('project_id').unsigned();
+      table.foreign('project_id')
+        .references('projects.id')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+    })
+  ])
 };
 
-exports.down = function(knex) {
+exports.down = knex => {
+  return Promise.all([
+    knex.schema.dropTable('palettes'),
+    knex.schema.dropTable('projects')
+  ])
   
 };
