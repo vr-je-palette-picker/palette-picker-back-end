b312630513d44e0fe0221a74d7dd4d266c21bdd3
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2019-12-03 15:38:10 -0700

    Installed knex, update knexfile, and made initial migration
    Co-authored-by: Vanessa Randall <vanessalrandall@gmail.com>

---
+++ knexfile.js
@@ -0,0 +1,24 @@
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/palette_picker',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
+  }
+};
