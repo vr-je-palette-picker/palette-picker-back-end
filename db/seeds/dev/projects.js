acdfecba91345c7a1080a86d418b69f6aed256d0
Vanessa Randall <vanessalrandall@gmail.com>
2019-12-03 17:34:39 -0700

    Fix logic in seed file to allow palettes to be iterated through to contain the correct project id
    Co-authored-by: Jeannie Evans <jeanmarieevans3@gmail.com>

--- db/seeds/dev/projects.js
+++ db/seeds/dev/projects.js
@@ -12,17 +12,19 @@ const createProject = (knex, project) => {
       let palettePromises = [];
 
       data.palettes.forEach(palette => {
-        palettePromises.push(
-          createPalette(knex, {
-            palette_name: palette.palette_name,
-            color_1: palette.color_1,
-            color_2: palette.color_2,
-            color_3: palette.color_3,
-            color_4: palette.color_4,
-            color_5: palette.color_5,
-            project_id: project_id[0]
-          })
-        );
+        if (palette.project_name === project.project_name) {
+          palettePromises.push(
+            createPalette(knex, {
+              palette_name: palette.palette_name,
+              color_1: palette.color_1,
+              color_2: palette.color_2,
+              color_3: palette.color_3,
+              color_4: palette.color_4,
+              color_5: palette.color_5,
+              project_id: project_id[0]
+            })
+          );
+        }
       });
 
       return Promise.all(palettePromises);
