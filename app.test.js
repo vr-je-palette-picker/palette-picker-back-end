a76052527f96010afe3533898c6576a6bc4010e8
Vanessa Randall <vanessalrandall@gmail.com>
2019-12-05 16:22:24 -0700

    Add testing coverage for DELETE endpoints, all palette and palettes endpoints and add error testing coverage on PATCH projects endpoint
    Co-authored-by: Jeannie Evans <jeanmarieevans3@gmail.com>

--- app.test.js
+++ app.test.js
@@ -170,20 +170,121 @@ describe('Server', () => {
   });
 
   describe('PATCH /api/v1/projects/:id', () => {
-    it('should return a 202 status code and update the project name', async () => {
+    it('should return a 200 status code and update the project name', async () => {
       const expectedProject = await database('projects').first();
       const { id } = expectedProject;
 
-      const newProject = { project_name: 'Tea and Crumpets' };
+      const updatedProject = { project_name: 'Tea and Crumpets' };
 
       const response = await request(app)
         .patch(`/api/v1/projects/${id}`)
-        .send(newProject);
+        .send(updatedProject);
       const projects = await database('projects').where('id', id);
       const project = projects[0];
 
       expect(response.status).toBe(200);
-      expect(project.project_name).toBe(newProject.project_name);
+      expect(project.project_name).toBe(updatedProject.project_name);
     });
+
+    it('should return a 422 status code and an error message', async () => {
+      const expectedProject = await database('project').first();
+      const newName = { projectName: 'Tea and Crumpets' };
+      
+      const response = await request(app).patch(`/api/v1/projects/${expectedProject.id}`).send(newName);
+
+      expect(response.status).toBe(422);
+      expect(response.body.error).toBe('PATCH failed, missing required key: project_name')
+    })
+
+    it('should return a 404 status code and an error message', async () => {
+      const invalidId = -1;
+      
+      const response = await request(app).patch(`/api/v1/projects/${invalidId}`);
+
+      expect(response.status).toBe(404);
+      expect(response.body.error).toBe('This project does not exist')
+    })
   });
+
+  describe('PATCH /api/v1/palette/:id', () => {
+    it('should return a 200 status code and update the project param passed in', () => {
+      const expectedPalette = await database('palettes').first();
+      const newInfo = {
+        palette_name: 'Fresh Fall',
+        color_2: '#60463B'
+      }
+
+      const response = await request(app)
+        .patch(`/api/v1/palette/${expectedPalette.id}`)
+        .send(newInfo);
+      const updatedPalette = await database('palettes').first();
+
+      expect(expectedPalette.palette_name).toBe('Option 1');
+      expect(response.status).toBe(200);
+      expect(updatedPalette.color_2).toBe(newInfo.color_2);
+    });
+
+    it('should return a 422 status code and an error message', async () => {
+      const expectedPalette = await database('palettes').first();
+      const newPaletteName = { paletteName: 'Kitchen Walls' }
+      
+      const response = await request(app).patch(`/api/v1/palette/${expectedPalette.id}`).send(newPaletteName);
+
+      expect(response.status).toBe(422);
+      expect(response.body.error).toBe('PATCH failed, missing required key')
+    })
+
+    it('should return a 404 status code and error message', async () => {
+      const invalidId = -1;
+      
+      const response = await request(app).patch(`/api/v1/palette/${invalidId}`);
+
+      expect(response.status).toBe(404);
+      expect(response.body.error).toBe('This palette does not exist')
+    })
+  })
+
+  describe('DELETE /api/v1/projects/:id', () => {
+    it('should return a 200 status code and remove project from database', async () => {
+      const currentProjects = await database('projects').select();
+      const projectToDelete = await database('projects').first();
+
+      const response = await request(app).delete(`/api/v1/projects/${projectToDelete.id}`)
+      const updatedProjects = await database('projects').select();
+      const updatedPalettes = await database('palettes').where( 'project_id', projectToDelete.id);
+
+      expect(response.status).toBe(200);
+      expect(updatedProjects.length).toBe(currentProjects.length - 1);
+      expect(updatedPalettes.length).toBe(0);
+    })
+
+    it('should return a 404 status code and an error message when there is no project found', async () => {
+      const response = await request(app).delete('/api/v1/projects/-1');
+      const expectedMessage = 'No project with this id can be found';
+
+      expect(response.status).toBe(404);
+      expect(response.body.error).toBe(expectedMessage)
+    })
+  })
+
+  describe('DELETE /api/v1/palette/:id', () => {
+    it('should return a 200 status code and remove the palette from the project and the database', async () => {
+      const currentPalettes = await database('palettes').select();
+      const paletteToDelete = await database('palettes').first();
+
+      const response =  await request(app).delete(`/api/v1/palette/${paletteToDelete.id}`);
+      const updatedPalettes = await database('palettes').select();
+
+      expect(response.status).toBe(200);
+      expect(updatedPalettes.length).toBe(currentPalettes.length - 1);
+    })
+
+    it('should return a 404 status code and an error message when there is no palette found', async () => {
+      const response = await request(app).delete('/api/v1/palette/-1');
+      const expectedMessage = 'No palette with this id can be found';
+
+      expect(response.status).toBe(404);
+      expect(response.body.error).toBe(expectedMessage);
+    })
+  })
 });
