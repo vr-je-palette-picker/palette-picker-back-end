ae51c37a372a789e343060dfd73d7ed371c7bdf9
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2019-12-04 17:31:34 -0700

    Add PATCH endpoint and update test for it to pass

--- app.js
+++ app.js
@@ -55,7 +55,6 @@ app.post('/api/v1/projects', async (request, response) => {
 
   try {
     const newProject = await database('projects').insert(project, 'id')
-    
     response.status(201).json({ id: newProject[0] })
   } catch (error) {
     response.status(500).json({ error: 'Internal server error' })
@@ -63,15 +62,40 @@ app.post('/api/v1/projects', async (request, response) => {
 });
 
 app.patch('/api/v1/projects/:id', async (request, response) => {
-  const project = request.body;
+  const { project_name } = request.body;
+  const { id } = request.params;
 
-  if (!project.project_name) {
-    return response.status(422).send({ error: 'POST failed, missing required key: project_name' });
+  if (!project_name) {
+    return response.status(422).send({ error: 'PATCH failed, missing required key: project_name' });
   }
 
   try {
-    const newProject 
+    const project = await database('projects').where({ id }).update({ project_name })
+    
+    if (project) {
+      return response.status(200).json({ project })
+    } else {
+      return response.status(404).json({ error: 'This project does not exist' })
+    }
+  } catch (error) {
+    response.status(500).json({ error: 'Internal server error '})
   }
+
 });
 
 module.exports = app;
+
+// app.patch('/api/v1/projects/:name', (request, response) => {
+//   const { name } = request.params;
+//   database('projects').where({ name })
+//     .then(project => {  
+//       if (!project.length) {
+//         return response.status(404).json({ error: `No existing project with name of ${name}`})
+//       }
+//       database('projects').where({ name }).update({
+//         name: request.body.name
+//       })
+//     .then(() => response.status(202).json({ message: `Project name changed to ${request.body.name}`}))
+//     })
+//   .catch((error) => response.status(500).json({ error }))
+// })
